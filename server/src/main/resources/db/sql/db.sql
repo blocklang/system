abf5660c3cf362e1dce3484bccd66667899370fa
xiaohulu <idocument@qq.com>
2020-03-14 17:06:13 +0800

    test: 新增开发环境和测试环境

--- server/src/main/resources/db/sql/db.sql
+++ server/src/main/resources/db/sql/db.sql
@@ -1,3 +1,4 @@
+-- dev db
 CREATE DATABASE system
     WITH 
     OWNER = postgres
@@ -5,4 +6,15 @@ CREATE DATABASE system
     CONNECTION LIMIT = -1;
 
 COMMENT ON DATABASE system
-    IS '系统管理';
+    IS '系统管理开发数据库';
+
+
+-- test db
+CREATE DATABASE system_test
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE system_test
+    IS '系统管理测试数据库';
